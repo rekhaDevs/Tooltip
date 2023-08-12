f9947e88d964b48d3fb356f5d7ae50faeff0c42d
Rekha <rekha.sharma@luezoid.com>
2023-08-12 13:12:32 +0530

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomTooltipComponent } from './components/custom-tooltip/custom-tooltip.component';
 import { CustomTooltipDirective } from './customdirectives/custom-tooltip.directive';
-import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomTooltipComponent,
-    CustomTooltipDirective,
-    MainComponent
+    CustomTooltipDirective
   ],
   imports: [
     BrowserModule,
--- src/app/customdirectives/custom-tooltip.directive.ts
+++ src/app/customdirectives/custom-tooltip.directive.ts
@@ -1,4 +1,5 @@
-import { ApplicationRef, ComponentFactoryResolver, ComponentRef, Directive, ElementRef, HostListener, Injector, Input } from '@angular/core';
+import { ApplicationRef, ComponentFactoryResolver, ComponentRef, Directive, ElementRef, EmbeddedViewRef, HostListener, Injector, Input } from '@angular/core';
+import { CustomTooltipComponent } from '../components/custom-tooltip/custom-tooltip.component';
 import { customTooltipConfig } from '../Interfaces/customtooltipInterface';
 
 @Directive({
@@ -8,18 +9,33 @@ export class CustomTooltipDirective {
   @Input() appCustomTooltip = '';
   private componentRef: ComponentRef<any> = null;
   constructor( private elementRef: ElementRef, 
-    private apppRef: ApplicationRef,
+    private appRef: ApplicationRef,
     private componentFactoryResolver: ComponentFactoryResolver,
     private injector: Injector) { }
 
   @HostListener('mouseenter')
-  onMouseEnter() : void{
-   if(this.componentRef === null){
-
-   }
+  onMouseEnter(): void {
+    if (this.componentRef === null) {
+        const componentFactory =
+              this.componentFactoryResolver.resolveComponentFactory(
+              CustomTooltipComponent);
+        this.componentRef = componentFactory.create(this.injector);
+        this.appRef.attachView(this.componentRef.hostView);
+        const domElem = 
+              (this.componentRef.hostView as EmbeddedViewRef<any>)
+              .rootNodes[0] as HTMLElement;       
+        document.body.appendChild(domElem);
+        this.setTooltipComponentProperties();
+    }
   }
   
-  private showTooltip(){
-    
+  private setTooltipComponentProperties() {
+    if (this.componentRef !== null) {
+      this.componentRef.instance.tooltip = this.appCustomTooltip;
+      const {left, right, bottom} = 		  	
+            this.elementRef.nativeElement.getBoundingClientRect();
+      this.componentRef.instance.left = (right - left) / 2 + left;
+      this.componentRef.instance.top = bottom;
+    }
   }
 }
